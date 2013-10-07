51862d0bf9fd164ee542c44152cc7ed7e7f31966
Juan Tabares <juanctt@Juans-Mac.local>
2013-10-07 22:16:59 +0900

    added the keys

---
+++ KanjiMe/KanjiMe/Models/AdMobLoader.h
@@ -0,0 +1,17 @@
+//
+//  AdMobLoader.h
+//  KanjiMe
+//
+//  Created by Lion User on 9/13/13.
+//  Copyright (c) 2013 Alteran System. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GADBannerView.h"
+
+
+@interface AdMobLoader : NSObject
++ (GADBannerView *)getNewBannerView:(UIViewController *)mainView;
++ (GADRequest *)getNewRequest:(BOOL)testMode;
+
+@end
